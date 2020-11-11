f5f61b94706ee34044ae28d11d016402246fac60
jpenagossura <jpenagos@sura.com.co>
2020-11-11 10:58:07 -0500

    modificacion pagina principal

--- react-2/src/App.js
+++ react-2/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
 import './App.css';
-import Vehiculos from "./Vehiculos";
+import Pagina from "./Pagina";
 
 function App() {
-  return <Vehiculos />;
+  return <Pagina />;
 }
 
 export default App;
